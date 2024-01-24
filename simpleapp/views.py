42460e94de23eba51eab08c4d3a460e0dbdb2855
Ilya Stritskiy <msv_ilya@mail.ru>
2024-01-24 21:23:56 +0300

    was added checking for permissions

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -12,17 +12,17 @@ from .models import Product
 
 
 # Create your views here.
-def multiply(request):
-    number = request.GET.get('number')
-    multiplier = request.GET.get('multiplier')
-
-    try:
-        result = int(number) * int(multiplier)
-        html = f"<html><body>{number}*{multiplier}={result}</body></html>"
-    except (ValueError, TypeError):
-        html = f"<html><body>Invalid input.</body></html>"
-
-    return HttpResponse(html)
+# def multiply(request):
+#     number = request.GET.get('number')
+#     multiplier = request.GET.get('multiplier')
+#
+#     try:
+#         result = int(number) * int(multiplier)
+#         html = f"<html><body>{number}*{multiplier}={result}</body></html>"
+#     except (ValueError, TypeError):
+#         html = f"<html><body>Invalid input.</body></html>"
+#
+#     return HttpResponse(html)
 
 
 class ProductsList(ListView):
