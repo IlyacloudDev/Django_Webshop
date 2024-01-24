180c1a80e70c93f60baeaef9e93d46f5dab64a28
Ilya Stritskiy <msv_ilya@mail.ru>
2024-01-24 21:21:54 +0300

    was added new functional for sending emails

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,11 @@
 # from django import forms
 # from django.contrib.auth.forms import UserCreationForm
 # from django.contrib.auth.models import User
+
+
 from allauth.account.forms import SignupForm
 from django.contrib.auth.models import Group
+from django.core.mail import EmailMultiAlternatives
 
 
 # создали кастомный сопсоб регистрации на основе существующего в пакете allauth
@@ -10,9 +13,22 @@ class CustomSignupForm(SignupForm):
     def save(self, request):
         user = super().save(request)
         common_users = Group.objects.get(name="common users")
-        user.groups.add(common_users)
+        user.groups.add(common_users)  # теперь при успешной регистрации (только если через email и пароль) пользователь будет добавляться в группу common users
+
+        # механизм отправки письма нашему успешно зарегестрированному пользователю
+        subject = 'Добро пожаловать в наш интернет-магазин!'
+        text = f'{user.username}, вы успешно зарегистрировались на сайте!'
+        html = (
+            f'<b>{user.username}</b>, вы успешно зарегистрировались на '\
+            f'<a href="http://127.0.0.1:8000/products">сайте</a>!'
+        )
+        msg = EmailMultiAlternatives(
+            subject=subject, body=text, from_email=None, to=[user.email]
+        )
+        msg.attach_alternative(html, "text/html")
+        msg.send()
+
         return user
-        # теперь при успешной регистрации (только если через email и пароль) пользователь будет добавляться в группу common users
 
 
 # тут мы создавали собственный вариант регистрации
